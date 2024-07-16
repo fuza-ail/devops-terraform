fc023a1f39a932a9eb61fb7404bd5e5a5bb6a7de
fuzail <fuzail@kali>
2024-07-16 20:53:47 +0700

    feat: create module

--- main.tf
+++ main.tf
@@ -12,29 +12,10 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "ec2_instance" {
+  source = "./instance"
 }
 
-resource "aws_instance" "example_terraform" {
-  ami = "ami-0c185732ad1b6169b"
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+module "s3-bucket" {
+  source = "./s3-bucket"
 }
-
